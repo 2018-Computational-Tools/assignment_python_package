fadd6222c70e2ad7cc6bc258cff1d89eafbb30c1
Sebastian Kopf <sebastian.kopf@colorado.edu>
2018-10-04 20:54:19 -0600

    simplify setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='0.1.0',
     description='',
     url='ttps://github.com/USER/REPO',
-    packages=['awesome'],
+    packages=find_packages(where="src"),
     package_dir={'': 'src'},
     install_requires=['numpy'],
     entry_points={'console_scripts': ['hello = awesome.module:hello']},
