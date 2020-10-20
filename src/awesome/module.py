c24064d5770cc59c3521d6e26afe2623ce01c250
Sebastian Kopf <sebastian.kopf@colorado.edu>
2020-10-20 15:40:59 -0600

    Update module.py

--- src/awesome/module.py
+++ src/awesome/module.py
@@ -6,3 +6,28 @@ def hello():
     This function says hello and reports the current version of this package.
     """
     return ('Hello, my newest version is ' + __version__)
+
+def add(x, y):
+    """Add things!
+    Function to add things.
+
+    :param x: first number
+    :type x: float
+
+    :param y: second number
+    :type y: float
+
+    :return: addition of x and y
+    :rtype: float
+
+    >>> add(1, 1)
+    2
+    """
+    return (float(x) + float(y))
+
+
+def add_cmd():
+    """command line wrapper for add function"""
+    import sys
+    arg1, arg2 = sys.argv[1], sys.argv[2]
+    print("Hello friend! " + arg1 + " + " + arg2 + " = " + str(add(arg1, arg2)))
